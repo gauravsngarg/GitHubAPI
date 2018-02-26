4516b6401e141999495b542aea368037d943d90a
Gaurav Garg <gauravsngarg@gmail.com>
2018-02-26 22:15:30 +0400

    User Activity UI Part

--- app/src/main/java/gauravsngarg/com/githubapi/UserActivity.java
+++ app/src/main/java/gauravsngarg/com/githubapi/UserActivity.java
@@ -1,13 +1,51 @@
 package gauravsngarg.com.githubapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UserActivity extends AppCompatActivity {
+
+    public static final String USERNAMEINFO = "usernameinfo";
+
+    @BindView(R.id.avatar)
+    ImageView avatarImg;
+
+    @BindView(R.id.userName)
+    TextView userNameTV;
+
+    @BindView(R.id.followers)
+    TextView followersTV;
+
+    @BindView(R.id.following)
+    TextView followingTV;
+
+    @BindView(R.id.email)
+    TextView email;
+
+    @BindView(R.id.logIn)
+    TextView logIn;
+
+    @BindView(R.id.ownedReposBtn)
+    Button ownedrepos;
+
+    Bundle extras;
+
+    String userNameEntered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        extras = getIntent().getExtras();
+        userNameEntered = extras.getString(USERNAMEINFO);
+
+        ButterKnife.bind(this);
     }
 }
