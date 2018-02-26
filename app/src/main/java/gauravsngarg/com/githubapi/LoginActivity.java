cacb6299a140c9930a8d935148bfc4bf3d2af749
Gaurav Garg <gauravsngarg@gmail.com>
2018-02-26 21:44:55 +0400

    Login User Screen and User Info Screen

--- app/src/main/java/gauravsngarg/com/githubapi/LoginActivity.java
+++ app/src/main/java/gauravsngarg/com/githubapi/LoginActivity.java
@@ -1,13 +1,41 @@
 package gauravsngarg.com.githubapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String USERNAMEINFO = "usernameinfo";
+
+    @BindView(R.id.input_username)
+    EditText username;
+
+    @BindView(R.id.btn_login)
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_login){
+            Intent i = new Intent(LoginActivity.this, UserActivity.class);
+            i.putExtra(USERNAMEINFO, username.getText().toString());
+            startActivity(i);
+        }
     }
 }
